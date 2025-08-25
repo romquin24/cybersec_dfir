6d79cff3c12919d1d149b824c608a558549ad2a3
romquin24 <rommel.quindoyos@pfda.gov.ph>
2025-08-25 15:43:49 +0800

    Add files via upload

---
+++ mainitnakey.ps1
@@ -0,0 +1,10 @@
+﻿Implementition Data Confidentiality
+Using Public keys:
+@@corebaba
+config t
+username admin privilege 15 secret pass
+username rommel privilege 15 secret pass
+ip domain-name pfda.gov.ph
+crypto  key generate rsa
+ip ssh version 2
+ip ssh time-out 120
